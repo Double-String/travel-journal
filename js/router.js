748d2f3fb011f7f7ba319338ba8b3f4f681fe38e
dani <moitidani@gmail.com>
2024-02-19 14:15:10 -0600

    new edits

--- js/router.js
+++ js/router.js
@@ -7,17 +7,16 @@ export class Router {
 
     route(event) {
         console.log("Route event activated")
-        event = event || window.event
         event.preventDefault()
-
+    
         window.history.pushState({}, "", event.target.href);
-
+    
         this.handle()
     }
 
     handle() {
         const { pathname } = window.location
-        const route = this.routes[pathname]
+        const route = this.routes[pathname] || this.routes[404];
 
         fetch(route)
             .then(data => data.text())

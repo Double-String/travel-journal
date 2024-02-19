d24680aa30d6adf35a2e19bb2b6206f007f2b209
dani <moitidani@gmail.com>
2024-02-19 14:49:58 -0600

    fixed duplicated code

--- js/router.js
+++ js/router.js
@@ -23,17 +23,5 @@ export class Router {
         .then((html) => {
           document.querySelector("#app").innerHTML = html;
         });
-  
-      console.log(route);
-        const { pathname } = window.location
-        const route = this.routes[pathname] || this.routes[404];
-
-        fetch(route)
-            .then(data => data.text())
-            .then(html => {
-                document.querySelector("#app").innerHTML = html;
-            });
-
-        console.log(route);
     }
 }
